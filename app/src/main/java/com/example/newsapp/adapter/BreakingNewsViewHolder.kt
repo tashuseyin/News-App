208428c7d792701b7a7c9316c709c4666d506de0
Huseyin Tas <htas767@gmail.com>
2021-06-10 11:50:00 +0300

    [WIP] News Source Converter

--- app/src/main/java/com/example/newsapp/adapter/BreakingNewsViewHolder.kt
+++ app/src/main/java/com/example/newsapp/adapter/BreakingNewsViewHolder.kt
@@ -8,7 +8,8 @@ import com.example.newsapp.model.Article
 class BreakingNewsViewHolder(private val binding: BreakingRowitemBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(article: Article) {
-        Glide.with(binding.imageBreaking).load(article.urlToImage).into(binding.imageBreaking)
+        Glide.with(binding.imageBreaking).load(article.urlToImage)
+            .into(binding.imageBreaking)
         binding.breakingTitle.text = article.title
         binding.breakingDescription.text = article.description
     }
